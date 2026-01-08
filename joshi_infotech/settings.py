d5d2924a7329fefa079a6e76c5bbbc39e37522bf
sukheshkulkarni-crypto <sukhesh.kulkarni@gmail.com>
2026-01-08 12:19:17 +0530

    Update settings.py

--- joshi_infotech/settings.py
+++ joshi_infotech/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-fwo!#ei75+oef4nvtr@1jh(@gfhf5!gmg0^dug5m#qnq)82)&@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -136,3 +136,4 @@ STATICFILES_DIRS=[BASE_DIR/"static"]
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
